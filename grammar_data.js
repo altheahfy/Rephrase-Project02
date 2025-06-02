e6835f28803f9907e635adda41fc4b4ce57137ba
altheahfy <altheahfy@gmail.com>
2025-06-02 15:53:47 +0900

    全体ランダマイズテスト用データjs作成

---
+++ grammar_data.js
@@ -0,0 +1,37 @@
+export const grammarData = {
+  "010": {
+    "A": ["want", "hope", "plan"],
+    "B": {
+      "want": {
+        subject: ["I", "You", "They"],
+        object1: ["to go", "to eat", "to sleep"],
+        object2: [],
+        complement: [],
+        m1: ["tomorrow", "soon", "now"],
+        m2: [],
+        m3: [],
+        aux: ["would", "might", "can"]
+      },
+      "hope": {
+        subject: ["She", "We"],
+        object1: ["to win", "to find it"],
+        object2: [],
+        complement: [],
+        m1: ["next time", "by Friday"],
+        m2: [],
+        m3: [],
+        aux: ["can", "will"]
+      },
+      "plan": {
+        subject: ["They", "My team"],
+        object1: ["to start", "to leave early"],
+        object2: [],
+        complement: [],
+        m1: ["this week", "soon"],
+        m2: [],
+        m3: [],
+        aux: ["must", "may"]
+      }
+    }
+  }
+};
