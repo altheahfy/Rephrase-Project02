4ef3e242fb09498c76ebcab9236478d246757914
altheahfy <altheahfy@gmail.com>
2025-06-03 16:04:51 +0900

    subslot対応１７（グローバル登録）

--- subslot_renderer.js
+++ subslot_renderer.js
@@ -109,3 +109,7 @@ function generateEmptyStructure() {
 function randomizeSubSlot(slotKey) {
     console.log(`Randomize requested for: ${slotKey}`);
 }
+
+
+// グローバルスコープに登録
+window.renderSubSlots = renderSubSlots;
