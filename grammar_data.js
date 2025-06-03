b5f4c09a9992d3ce16a93466324ac7d52e5f1871
altheahfy <altheahfy@gmail.com>
2025-06-03 20:08:49 +0900

    subslot対応２３（実データ入り再帰構造）

--- grammar_data.js
+++ grammar_data.js
@@ -1,60 +1,156 @@
 window.grammarData = {
   "010": {
-    A: ["want", "hope", "plan"],
-    B: { /* ...省略（前回同様）... */ },
     embedded_structure: {
       m1: {
-        imagePath: "slot_images/common/soon.png",
-        subslots: generateAllSubslots()
+        imagePath: "slot_images/common/sample_m1.png",
+        subslots: {
+          m1: { imagePath: "slot_images/common/sub_m1.png" },
+          s:  { text: "サブ主語" },
+          aux: { text: "サブ助動詞" },
+          m2: { imagePath: "slot_images/common/sub_m2.png" },
+          v:  { imagePath: "slot_images/common/sub_v.png" },
+          c:  { text: "サブC" },
+          o1: { imagePath: "slot_images/common/sub_o1.png" },
+          o2: { text: "サブO2" },
+          c2: { imagePath: "slot_images/common/sub_c2.png" },
+          m3: { text: "サブM3" }
+        }
       },
       s: {
-        imagePath: "slot_images/common/you.png",
-        subslots: generateAllSubslots()
+        imagePath: "slot_images/common/sample_s.png",
+        subslots: {
+          m1: { text: "S階層のm1" },
+          s:  { imagePath: "slot_images/common/sub_s.png" },
+          aux: { imagePath: "slot_images/common/sub_aux.png" },
+          m2: { text: "S階層のm2" },
+          v:  { text: "S階層のv" },
+          c:  { imagePath: "slot_images/common/sub_c.png" },
+          o1: { text: "S階層のo1" },
+          o2: { imagePath: "slot_images/common/sub_o2.png" },
+          c2: { text: "S階層のc2" },
+          m3: { imagePath: "slot_images/common/sub_m3.png" }
+        }
       },
       aux: {
-        text: "するつもりだった",
-        subslots: generateAllSubslots()
+        text: "助動詞",
+        subslots: {
+          m1: { text: "AUX階層のm1" },
+          s: { imagePath: "slot_images/common/sub_s.png" },
+          aux: { text: "AUX階層のaux" },
+          m2: { imagePath: "slot_images/common/sub_m2.png" },
+          v: { text: "AUX階層のv" },
+          c: { imagePath: "slot_images/common/sub_c.png" },
+          o1: { text: "AUX階層のo1" },
+          o2: { imagePath: "slot_images/common/sub_o2.png" },
+          c2: { imagePath: "slot_images/common/sub_c2.png" },
+          m3: { text: "AUX階層のm3" }
+        }
       },
       m2: {
-        subslots: generateAllSubslots()
+        text: "M2",
+        subslots: {
+          m1: { imagePath: "slot_images/common/sub_m1.png" },
+          s: { text: "M2階層のs" },
+          aux: { imagePath: "slot_images/common/sub_aux.png" },
+          m2: { text: "M2階層のm2" },
+          v: { imagePath: "slot_images/common/sub_v.png" },
+          c: { text: "M2階層のc" },
+          o1: { imagePath: "slot_images/common/sub_o1.png" },
+          o2: { text: "M2階層のo2" },
+          c2: { imagePath: "slot_images/common/sub_c2.png" },
+          m3: { imagePath: "slot_images/common/sub_m3.png" }
+        }
       },
       v: {
-        imagePath: "slot_images/common/want.png",
-        subslots: generateAllSubslots()
+        imagePath: "slot_images/common/sample_v.png",
+        subslots: {
+          m1: { text: "V階層のm1" },
+          s: { imagePath: "slot_images/common/sub_s.png" },
+          aux: { text: "V階層のaux" },
+          m2: { imagePath: "slot_images/common/sub_m2.png" },
+          v: { text: "V階層のv" },
+          c: { imagePath: "slot_images/common/sub_c.png" },
+          o1: { text: "V階層のo1" },
+          o2: { imagePath: "slot_images/common/sub_o2.png" },
+          c2: { text: "V階層のc2" },
+          m3: { imagePath: "slot_images/common/sub_m3.png" }
+        }
       },
       c: {
-        subslots: generateAllSubslots()
+        text: "C",
+        subslots: {
+          m1: { text: "C階層のm1" },
+          s: { imagePath: "slot_images/common/sub_s.png" },
+          aux: { text: "C階層のaux" },
+          m2: { imagePath: "slot_images/common/sub_m2.png" },
+          v: { imagePath: "slot_images/common/sub_v.png" },
+          c: { imagePath: "slot_images/common/sub_c.png" },
+          o1: { text: "C階層のo1" },
+          o2: { imagePath: "slot_images/common/sub_o2.png" },
+          c2: { text: "C階層のc2" },
+          m3: { imagePath: "slot_images/common/sub_m3.png" }
+        }
       },
       o1: {
-        imagePath: "slot_images/common/to_go.png",
-        subslots: generateAllSubslots()
+        imagePath: "slot_images/common/sample_o1.png",
+        subslots: {
+          m1: { text: "O1階層のm1" },
+          s: { imagePath: "slot_images/common/sub_s.png" },
+          aux: { text: "O1階層のaux" },
+          m2: { imagePath: "slot_images/common/sub_m2.png" },
+          v: { text: "O1階層のv" },
+          c: { imagePath: "slot_images/common/sub_c.png" },
+          o1: { text: "O1階層のo1" },
+          o2: { imagePath: "slot_images/common/sub_o2.png" },
+          c2: { text: "O1階層のc2" },
+          m3: { imagePath: "slot_images/common/sub_m3.png" }
+        }
       },
       o2: {
-        subslots: generateAllSubslots()
+        text: "O2",
+        subslots: {
+          m1: { imagePath: "slot_images/common/sub_m1.png" },
+          s: { text: "O2階層のs" },
+          aux: { imagePath: "slot_images/common/sub_aux.png" },
+          m2: { text: "O2階層のm2" },
+          v: { imagePath: "slot_images/common/sub_v.png" },
+          c: { text: "O2階層のc" },
+          o1: { imagePath: "slot_images/common/sub_o1.png" },
+          o2: { text: "O2階層のo2" },
+          c2: { imagePath: "slot_images/common/sub_c2.png" },
+          m3: { imagePath: "slot_images/common/sub_m3.png" }
+        }
       },
       c2: {
-        subslots: generateAllSubslots()
+        imagePath: "slot_images/common/sample_c2.png",
+        subslots: {
+          m1: { text: "C2階層のm1" },
+          s: { imagePath: "slot_images/common/sub_s.png" },
+          aux: { text: "C2階層のaux" },
+          m2: { imagePath: "slot_images/common/sub_m2.png" },
+          v: { text: "C2階層のv" },
+          c: { imagePath: "slot_images/common/sub_c.png" },
+          o1: { text: "C2階層のo1" },
+          o2: { imagePath: "slot_images/common/sub_o2.png" },
+          c2: { text: "C2階層のc2" },
+          m3: { imagePath: "slot_images/common/sub_m3.png" }
+        }
       },
       m3: {
-        imagePath: "slot_images/common/quickly.png",
-        subslots: generateAllSubslots()
+        imagePath: "slot_images/common/sample_m3.png",
+        subslots: {
+          m1: { text: "M3階層のm1" },
+          s: { imagePath: "slot_images/common/sub_s.png" },
+          aux: { text: "M3階層のaux" },
+          m2: { imagePath: "slot_images/common/sub_m2.png" },
+          v: { text: "M3階層のv" },
+          c: { imagePath: "slot_images/common/sub_c.png" },
+          o1: { text: "M3階層のo1" },
+          o2: { imagePath: "slot_images/common/sub_o2.png" },
+          c2: { text: "M3階層のc2" },
+          m3: { imagePath: "slot_images/common/sub_m3.png" }
+        }
       }
     }
   }
 };
-
-// ---- サンプル：全slotの空subslotを一括生成する関数 ----
-function generateAllSubslots() {
-  return {
-    m1: {},
-    s: {},
-    aux: {},
-    m2: {},
-    v: {},
-    c: {},
-    o1: {},
-    o2: {},
-    c2: {},
-    m3: {}
-  };
-}
