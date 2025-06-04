b463c618b80c8ee2bb338020c722e977e676d822
altheahfy <altheahfy@gmail.com>
2025-06-04 23:52:46 +0900

    動的描画テスト用

--- js/renderer_core.js
+++ js/renderer_core.js
@@ -1,7 +1,9 @@
 // renderer_core.js
+
 export function renderSlotImage(slotId, imagePath) {
   const img = document.querySelector(`#${slotId} img`);
   if (img) {
+    console.log(`描画中: ${slotId} → ${imagePath}`);
     img.src = imagePath;
     img.alt = `Image for ${slotId}`;
   }
