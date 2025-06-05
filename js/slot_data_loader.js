2dad490b4a09a00f2e223158da2345c5b382a50e
altheahfy <altheahfy@gmail.com>
2025-06-05 14:23:30 +0900

    C展開

--- js/slot_data_loader.js
+++ js/slot_data_loader.js
@@ -11,31 +11,24 @@ export async function loadSlotImageAndTextMap(structureId) {
   const slotImageMap = {};
   const slotTextMap = {};
   const slotKeys = ["m1", "s", "aux", "m2", "v", "c", "o1", "o2", "c2", "m3"];
+  const subPrefixes = ["o1", "c"]; // 展開対象subslot接頭辞
 
   slotKeys.forEach(key => {
-    // 上位スロットID
     const upperId = `slot-${key}`;
     const upperData = data[upperId];
     if (upperData) {
-      if (upperData.image) {
-        slotImageMap[upperId] = upperData.image;
-      }
-      if (upperData.text) {
-        slotTextMap[upperId] = upperData.text;
-      }
+      if (upperData.image) slotImageMap[upperId] = upperData.image;
+      if (upperData.text) slotTextMap[upperId] = upperData.text;
     }
 
-    // subslot ID
-    const subId = `slot-o1-sub-${key}`;
-    const subData = data[subId];
-    if (subData) {
-      if (subData.image) {
-        slotImageMap[subId] = subData.image;
+    subPrefixes.forEach(prefix => {
+      const subId = `slot-${prefix}-sub-${key}`;
+      const subData = data[subId];
+      if (subData) {
+        if (subData.image) slotImageMap[subId] = subData.image;
+        if (subData.text) slotTextMap[subId] = subData.text;
       }
-      if (subData.text) {
-        slotTextMap[subId] = subData.text;
-      }
-    }
+    });
   });
 
   return {
