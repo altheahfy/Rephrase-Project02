bb929ef7fe3d35af16114b8ad4864ec5e4c03cf3
altheahfy <altheahfy@gmail.com>
2025-06-24 09:16:30 +0900

    Revert "M1がランダマイズ"
    This reverts commit be132e63acead2c706c243d79eb5a8d51f1dfff0.

--- js/randomizer_all.js
+++ js/randomizer_all.js
@@ -72,18 +72,6 @@ export function randomizeAll(slotData) {
     }
   }
 
-  // Ensure M1 is selected using the same logic as other slots (randomly from example sets)
-  const m1Entries = groupSlots.filter(e => e.Slot === "M1");
-  exampleIDs.forEach(id => {
-    const m1InSet = m1Entries.filter(e => e.例文ID === id);
-    if (m1InSet.length > 0) {
-      const chosenM1 = m1InSet[Math.floor(Math.random() * m1InSet.length)];
-      selectedSlots.push({ ...chosenM1 });
-      const m1Subslots = groupSlots.filter(e => e.例文ID === chosenM1.例文ID && e.Slot === chosenM1.Slot && e.SubslotID);
-      m1Subslots.forEach(sub => selectedSlots.push({ ...sub }));
-    }
-  });
-
   window.slotSets = slotSets;
   window.slotTypes = slotTypes;
   window.lastSelectedSlots = selectedSlots;
