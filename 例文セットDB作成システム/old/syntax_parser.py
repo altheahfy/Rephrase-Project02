59dd242c1e0003eb128184896cacc568089ef96c
altheahfy <altheahfy@gmail.com>
2025-08-05 20:42:33 +0900

    Remove legacy and main scripts from project
    Deleted multiple Python scripts, data files, and old backup files from the project, including main modules and historical versions in the 'old' directory. This cleanup removes obsolete code and data to streamline the repository.

--- 例文セットDB作成システム/old/syntax_parser.py
+++
@@ -1,9 +0,0 @@
-
-import spacy
-from rephrase_mapper import map_to_rephrase_slots
-
-nlp = spacy.load("en_core_web_sm")
-
-def analyze_slotphrase(slotphrase):
-    doc = nlp(slotphrase)
-    return map_to_rephrase_slots(doc)
