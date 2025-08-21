25bd7e3dd83c720881c18c09af9a5d8571308761
altheahfy <altheahfy@gmail.com>
2025-08-21 23:54:56 +0900

    Revert "Add Phase 1 Universal Slot Position System core"
    This reverts commit d62486928fd49493ce44d74ee350a3e20a822ab5.

--- training/data/universal_slot_system/patterns/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Pattern implementations
-統一システム用パターン実装
-"""
-
-from .whose_pattern import WhosePattern
-from .passive_pattern import PassivePattern
-
-__all__ = ['WhosePattern', 'PassivePattern']
